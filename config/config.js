abbd7f7949d282f73507f4f7f0e7b6bfb2b9a600
Ryosuke <ryosuke.san.hana@gmail.com>
2018-08-08 14:52:02 -0700

    :wrench: Added S3 URL and common folders to config file

--- config/config.js
+++ config/config.js
@@ -1,5 +1,11 @@
 export default {
-    tokenName: 'kushyFToken',
-    kushyApiUrl: 'http://localhost/api/v1',
-    kushyLogin: 'http://127.0.0.1/oauth/authorize/?client_id=2&redirect_uri=http://127.0.0.1:3000/token&response_type=code&scope=access-user-account',
-}
+  tokenName: "kushyFToken",
+  kushyApiUrl: "http://localhost/api/v1",
+  kushyLogin:
+    "http://127.0.0.1/oauth/authorize/?client_id=2&redirect_uri=http://127.0.0.1:3000/token&response_type=code&scope=access-user-account",
+  assets: {
+    root: "https://kushy-frontend-assets.s3.amazonaws.com/",
+    site: "assets/",
+    uploads: "uploads/"
+  }
+};
