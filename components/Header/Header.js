fd34e4528555f73b1f49edf9c844d02d68c6ea03
Ryosuke <ryosuke.san.hana@gmail.com>
2018-08-08 16:41:39 -0700

    :bug: Header now references correct Redux user state

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -4,6 +4,7 @@ import {connect} from 'react-redux'
 import Link from 'next/link'
 import Router from 'next/router'
 import { Button, Dropdown, Menu } from 'semantic-ui-react'
+import config from "config/config"
 
 // import { Input } from 'semantic-ui-react'
 
@@ -13,55 +14,54 @@ import "./Header.css"
 
 class Header extends React.Component {
   state = {
-    activeItem: 'home'
+    activeItem: "home"
   };
-  
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
-  handleLogin = (e) => Router.push('http://127.0.0.1/oauth/authorize/?client_id=2&redirect_uri=http://127.0.0.1:3000/token&response_type=code&scope=access-user-account')
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
+  handleLogin = e => Router.push(config.kushyLogin);
 
   render() {
-    const { profile } = this.props;
+    const { user } = this.props;
     const { activeItem } = this.state;
-    // console.log(profile);
+    // console.log(user);
 
-    const userIcon = <img src="http://localhost:3000/static/assets/images/Icons/icon-user.svg" />
+    const userIcon = (
+      <img src="http://localhost:3000/static/assets/images/Icons/icon-user.svg" />
+    );
 
     return (
       <Menu color="red" fixed="top" inverted className="SiteHeader">
         <Link href="/" passHref>
-          <Menu.Item as="a" name='logo' active={activeItem === 'logo'}>
-            <i className="icon logo" alt="Kushy logo"></i>
+          <Menu.Item as="a" name="logo" active={activeItem === "logo"}>
+            <i className="icon logo" alt="Kushy logo" />
           </Menu.Item>
         </Link>
-        <Menu.Item
-          name='search'
-        >
+        <Menu.Item name="search">
           <div className="HeaderSearch tablet only">
             <HeaderSearch />
           </div>
         </Menu.Item>
 
-        <Menu.Menu position='right'>
-          {profile ? 
+        <Menu.Menu position="right">
+          {user ? (
             <Dropdown item trigger={userIcon}>
               <Dropdown.Menu>
-                <Link href={ '/dashboard/' } passHref>
-                  <Dropdown.Item>
-                      Dashboard
-                  </Dropdown.Item>
+                <Link href={"/dashboard/"} passHref>
+                  <Dropdown.Item>Dashboard</Dropdown.Item>
                 </Link>
-                  <Link href={ '/logout' } passHref>
-                    <Dropdown.Item>
-                        Logout
-                    </Dropdown.Item>
-                  </Link>
+                <Link href={"/logout"} passHref>
+                  <Dropdown.Item>Logout</Dropdown.Item>
+                </Link>
               </Dropdown.Menu>
             </Dropdown>
-          : 
+          ) : (
             <Menu.Item>
-                <Button primary onClick={ this.handleLogin }>Login</Button>
-            </Menu.Item>}
+              <Button primary onClick={this.handleLogin}>
+                Login
+              </Button>
+            </Menu.Item>
+          )}
         </Menu.Menu>
       </Menu>
     );
