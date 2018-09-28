f7f5497a1df3251179ce3cd737df5d5d183ba948
Ryosuke <ryosuke.san.hana@gmail.com>
2018-09-28 14:18:12 -0700

    :bug: HeaderSearch types regular speed + better timeout

--- components/Search/HeaderSearch/HeaderSearch.js
+++ components/Search/HeaderSearch/HeaderSearch.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { Search, Grid, Header, Segment } from 'semantic-ui-react'
 import KushyApi from 'utils/KushyApi'
 
-const api = new KushyApi()
 
 export default class SearchExampleStandard extends Component {
     constructor(props) {
@@ -15,65 +14,77 @@ export default class SearchExampleStandard extends Component {
     this.resetComponent()
   }
 
+    debounce = (func, wait = 100) => {
+        console.log('running debounce');
+
+        let timeout;
+        return function (...args) {
+            console.log('1 timing out function')
+
+            clearTimeout(timeout);
+            timeout = setTimeout(() => {
+                console.log('timing out function')
+                func.apply(this, args);
+            }, wait);
+        };
+    }
+
   resetComponent = () => this.setState({ isLoading: false, results: [], value: '' })
+
+  queryApi = () => {
+      console.log('querying api')
+      if (this.state.value.length < 1) return this.resetComponent()
+
+      // Query API for results
+      const api = new KushyApi()
+      api.search('name', this.state.value)
+          .then((results) => {
+              // let filteredResults = [];
+              let response = {
+              }
+              results.data.map((item) => {
+                  var section = item.section.toUpperCase() || 'Unknown'
+
+                  // create new section category
+                  if (response[section] === undefined) {
+                      response[section] = {
+                          name: section,
+                          results: []
+                      };
+                  }
+                  // add result to category
+                  response[section].results.push({
+                      title: item.name,
+                      image: item.featured_img,
+                      url: item.slug
+                  });
+
+                  // filteredResults.push({
+                  //     title: result.name,
+                  //     image: result.featured_img,
+                  //     category: result.categories[0].category.name
+                  // })
+              })
+              return response
+          })
+          .then((results) => {
+              this.setState({
+                  isLoading: false,
+                  results: results
+              })
+          })
+  }
 
   handleResultSelect = (e, { result }) => this.setState({ value: result.title })
 
   handleSearchChange = (e, { value }) => {
     this.setState({ isLoading: true, value })
-
-    setTimeout(() => {
-      if (this.state.value.length < 1) return this.resetComponent()
-
-      // Query API for results
-      api.search('name', this.state.value)
-        .then((results) => {
-            // let filteredResults = [];
-            let response = {
-                }
-            results.data.map((item) => {
-                var section = item.section.toUpperCase() || 'Unknown'
-
-                    // create new section category
-                    if (response[section] === undefined) {
-                        response[section] = {
-                            name: section,
-                            results: []
-                        };
-                    }
-                    // add result to category
-                    response[section].results.push({
-                        title: item.name,
-                        image: item.featured_img,
-                        url: item.slug
-                    });
-
-                // filteredResults.push({
-                //     title: result.name,
-                //     image: result.featured_img,
-                //     category: result.categories[0].category.name
-                // })
-            })
-            return response
-        })
-        .then((results) => {
-            this.setState({
-                isLoading: false,
-                results: results
-            })
-        })
-
-    }, 300)
-  }
-
-  debounce = (func, wait = 100) => {
-      let timeout;
-      return function (...args) {
-          clearTimeout(timeout);
-          timeout = setTimeout(() => {
-              func.apply(this, args);
-          }, wait);
-      };
+    
+    console.log('should be running debounce');
+      timeout = setTimeout(() => {
+          console.log('timing out function')
+          this.queryApi()
+      }, 420);
   }
 
   render() {
@@ -84,7 +95,7 @@ export default class SearchExampleStandard extends Component {
             category
             loading={isLoading}
             onResultSelect={this.handleResultSelect}
-            onSearchChange={this.debounce(this.handleSearchChange, 500, { leading: true })}
+            onSearchChange={this.handleSearchChange}
             results={results}
             value={value}
             {...this.props}
