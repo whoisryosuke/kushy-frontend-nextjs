d958df083bea788c8943f57999cda35babd65062
Ryosuke <ryosuke.san.hana@gmail.com>
2018-11-27 12:05:25 -0800

    :fire: Removed MUI from login page

--- pages/login.js
+++ pages/login.js
@@ -2,41 +2,6 @@ import React from 'react'
 import {connect} from 'react-redux'
 import compose from 'recompose/compose';
 import { userActions } from '../actions';
-import { withStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-
-import Header from '../containers/header';
-
-import '../assets/css/seshsource.css'
-
-const styles = theme => ({
-  root: {
-    flexGrow: 1,
-    zIndex: 1,
-    position: 'relative',
-    display: 'flex',
-  },
-  toolbar: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: '0 8px',
-    ...theme.mixins.toolbar,
-  },
-  content: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.default,
-    padding: theme.spacing.unit * 3,
-    paddingTop: '4rem'
-  },
-  paper: {
-    ...theme.mixins.gutters(),
-    paddingTop: theme.spacing.unit * 2,
-    paddingBottom: theme.spacing.unit * 2,
-  }
-});
 
 class Login extends React.Component {
   static getInitialProps ({ reduxStore, req }) {
@@ -69,18 +34,15 @@ class Login extends React.Component {
     const {dispatch} = this.props
 
     // dispatch login here
-    // login(dispatch, form.data)
     dispatch(userActions.login(this.state.username, this.state.password));
   }
 
   render () {
      const { classes, theme, loggedIn } = this.props;
     return (
-      <section className={classes.root}>
-        <Header />
-          <main className={classes.content}>
-            <div className={classes.toolbar} />
-            <Paper className={classes.paper} elevation={1}>
+      <section>
+          <main>
+            <div>
               <form onSubmit={ this.handleSubmit } className="ui form">
                   <section className="field">
                       <label>Username:</label>
@@ -101,25 +63,19 @@ class Login extends React.Component {
                           onChange={ this.handleChange } 
                       />
                   </section>
-                    <Button variant="contained" color="primary" onClick={ this.handleSubmit }>
+                    <button onClick={ this.handleSubmit }>
                       Submit
-                    </Button>
+                    </button>
               </form>
-            </Paper>
+            </div>
           </main> 
       </section>
     )
   }
 }
 
-// export default connect()(withStyles(styles, { withTheme: true })(Login))
-
 export default compose(
-  withStyles(styles, {
-    name: 'Login',
-    withTheme: true
-  }),
   connect(state => ({
-    uiTheme: state.theme,
+    profile: state.user.profile,
   })),
 )(Login);
